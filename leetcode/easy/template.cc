ea6525b04783a4d68830bd2402c4667d2c46e4c7
Neo Qian <qiansl127@gmail.com>
2015-06-20 19:31:03 +0800

    Add template file

---
+++ leetcode/easy/template.cc
@@ -0,0 +1,19 @@
+/*
+* # 
+* 
+*/
+
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+
+};
+
+int main() {
+    Solution *s = new Solution();
+
+    delete s;
+    return 0;
+}
